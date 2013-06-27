a0d48cdfd44550e0eb3c6852bef48d36a82bb85a
Oscar Aaron Revilla Escalona <aaron_revilla@hotmail.com>
2013-06-27 18:03:02 -0500

    Se agrego la clase que lee archivos

---
+++ ComputerGraphics/ComputerGraphics/LeerArchivo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ComputerGraphics
+{
+    class LeerArchivo
+    {
+        string rutaArchivo = null;
+
+        public LeerArchivo(string r)
+        {
+            rutaArchivo = r;
+            Console.WriteLine(this.rutaArchivo);
+        }
+
+        public string getLine(int line)
+        {
+            Console.WriteLine(rutaArchivo);
+            string[] lines = File.ReadAllLines(rutaArchivo);
+            return lines[line - 1];
+        }
+
+        public string[] getLines()
+        {
+            return File.ReadAllLines(rutaArchivo);
+        }
+
+        public string getAllDocument()
+        {
+            return File.ReadAllText(rutaArchivo);
+        }
+
+    }
+}
