f86dae5b20933231d61d5acd1e4ae6aba513ee6a
Oscar Aaron Revilla Escalona <aaron_revilla@hotmail.com>
2013-06-28 07:11:19 +0200

    Se agrego la vista para abrir documentos

---
+++ ComputerGraphics/ComputerGraphics/DrawLines.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ComputerGraphics
+{
+    /// <summary>
+    /// Lógica de interacción para DrawLines.xaml
+    /// </summary>
+    public partial class DrawLines : Window
+    {
+        public DrawLines()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var ofd = new Microsoft.Win32.OpenFileDialog();
+            var result = ofd.ShowDialog();
+            if (result == false) return;
+            nombreArchivo.Text = ofd.FileName;
+        }
+
+        
+    }
+}
