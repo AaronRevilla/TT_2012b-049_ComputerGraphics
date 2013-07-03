715750b6240a5b2e9530dc85537817e7c1957589
OmarHP <omar.hp90@gmail.com>
2013-07-03 15:11:34 -0500

    Commit 2 desde Visual Studio 2012

--- Trabajo_Terminal/Trabajo_Terminal/Borrar.cs
+++ Trabajo_Terminal/Trabajo_Terminal/Borrar.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Media;
 
 namespace GraphicNotes
 {
     class Borrar
     {
+        int atrib1;
+        Brush b = new SolidColorBrush(Color.FromRgb(0x1B, 0x4E, 0x79));
+
     }
 }
